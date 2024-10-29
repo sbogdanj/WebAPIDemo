02233912dfa3f8f4b472f66fd24422e7f497f128
Admin <Admin@DESKTOP-DFAIJ8K>
2024-10-29 12:22:39 +0200

    Add project files.

---
+++ WebAPIDemo/Controllers/ShirtsController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAPIDemo.Filters;
+using WebAPIDemo.Filters.ActionFilters;
+using WebAPIDemo.Filters.ExceptionFilters;
+using WebAPIDemo.Models;
+using WebAPIDemo.Models.Repositories;
+
+namespace WebAPIDemo.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ShirtsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetShirts()
+        {
+            return Ok(ShirtRepository.GetShirts());
+        }
+
+        [HttpGet("{id}")]
+        [Shirt_ValidateShirtIdFilter]
+        public IActionResult GetShirtById(int id)
+        {
+            return Ok(ShirtRepository.GetShirtById(id));
+        }
+
+        [HttpPost]
+        [Shirt_ValidateCreateShirtFilter]
+        public IActionResult CreateShirt([FromBody]Shirt shirt)
+        {
+            ShirtRepository.AddShirt(shirt);
+
+            return CreatedAtAction(nameof(GetShirtById),
+                new { id = shirt.ShirtId },
+                shirt);
+        }
+
+        [HttpPut("{id}")]
+        [Shirt_ValidateShirtIdFilter]
+        [Shirt_ValidateUpdateShirtFilter]
+        [Shirt_HandleUpdateExceptionsFilter]
+        public IActionResult UpdateShirt(int id, Shirt shirt)
+        {
+            ShirtRepository.UpdateShirt(shirt);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        [Shirt_ValidateShirtIdFilter]
+        public IActionResult DeleteShirt(int id)
+        {
+            var shirt = ShirtRepository.GetShirtById(id);
+            ShirtRepository.DeleteShirt(id);
+
+            return Ok(shirt);
+        }
+    }
+}
